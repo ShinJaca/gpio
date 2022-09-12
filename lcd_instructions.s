bf46606b635f3665f316ca929196b1efd2196cc2
ShinJaca <sergiopugli@gmail.com>
2022-09-12 21:16:45 +0000

    novas instruções

--- lcd_instructions.s
+++ lcd_instructions.s
@@ -3,6 +3,7 @@
 
 .equ    STMODE, 0x03
 .equ    B4MODE, 0x02
+.equ    B42LINE,0b00101000
 .equ    CLEAR,  0b00000001
 .equ    HOME,   0b00000010
 .equ    SHFTL,  0b00000111
@@ -12,6 +13,7 @@
 .equ    D1C1B1, 0b00001111  @ Display ON, Cursor ON, Blink ON
 
 .equ    CSHFTL, 0b00010000
+.equ    CSHFTR, 0b00010100
 .equ    STLNCH, 0x00101000  @ set 2 lines 5x7 matrix
 
 .equ    CGADDR, 0x01000000  @ Set address MASK
